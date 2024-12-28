b4e1db3235fba9272f6fa27500dde3a082020248
Jason Caldwell <jman8851@gmail.com>
2024-12-27 22:08:38 -0500

    marked Readme and ran some code experiements. Commented out experiments and wrote the code in js as assigned and confirmed code works by generating expected answers.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+//for (initialization; condition; increment/decrement) {
+    // logic
+//}
+
+//for (let i = 0; i < 5; i++) {
+  //  console.log(i);
+//}//
+
+//let i = 0;
+//while (i < 5) {
+  //  console.log(i);
+    //i++;
+//}    
+
+/*let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);*/
